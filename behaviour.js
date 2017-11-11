1aaec601986774fc6844951369abf97e09763476
JusticeMax <justicenefe@gmail.com>
2017-11-11 21:16:57 +0100

    Added smart thumbnail

--- behaviour.js
+++ behaviour.js
@@ -1,7 +1,37 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    var thumbnailElement = document.getElementById("smart_thumbnail");
+    let thumbnailElement = document.getElementById("smart_thumbnail");
     thumbnailElement.addEventListener('click', function () {
         // alert('I saw you click');
-        console.log('You clicked');
+        if (thumbnailElement.className === 'image-fun') {
+            thumbnailElement.className = '';
+        } else {
+            thumbnailElement.className = 'image-fun';
+        };
+    })
+
+
+
+
+
+
+
+    /**
+     * On Click, check if the menu is closed, so as to open it.
+     * also if open, so as to close it.
+     * This is to create a "toggle" effect;
+     */
+    let menuIcon = document.getElementById('mobile-menu-icon');
+    let menuList = document.getElementById('mobile-menu-links');
+    menuIcon.addEventListener('click', () => {
+        if (menuList.className.includes('isClosed')) {
+            // Open the menu;
+            menuIcon.className = 'ion-android-close';
+            menuList.className = 'menu-links';
+
+        } else {
+            // Close the menu;
+            menuIcon.className = 'ion-android-menu';
+            menuList.className = 'menu-links isClosed';
+        }
     })
 });
